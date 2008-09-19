a4df0c618db5623eb502c8c60ce63d7b2d377adc
djmamana <djmamana@2bfdcaec-5528-0410-9d21-f7f2b6497bdd>
2008-09-19 02:12:15 +0000

    git-svn-id: https://dynamicjasper.svn.sourceforge.net/svnroot/dynamicjasper/api/branches/dynamicjasper_v2_1@572 2bfdcaec-5528-0410-9d21-f7f2b6497bdd

--- src/main/java/ar/com/fdvs/dj/core/layout/ClassicLayoutManager.java
+++ src/main/java/ar/com/fdvs/dj/core/layout/ClassicLayoutManager.java
@@ -581,6 +581,7 @@ public class ClassicLayoutManager extends AbstractLayoutManager {
 			subreport.setHeight(100);
 			subreport.setPositionType(JRElement.POSITION_TYPE_FIX_RELATIVE_TO_TOP);
 			subreport.setStretchType(JRElement.STRETCH_TYPE_NO_STRETCH);
+			subreport.setRemoveLineWhenBlank(true);
 
 			if (sr.getStyle() != null)
 				applyStyleToElement(sr.getStyle(), subreport);
